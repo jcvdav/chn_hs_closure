500d67838361f1f0e0c8fc6dacb319626512e8f7
Juan Carlos Villasenor Derbez <juancarlos@ucsb.edu>
2020-12-01 12:44:24 -0800

    Added setup script and other dirs (wont show up on GH until they have stuff in them)

---
+++ scripts/00_setup.R
@@ -0,0 +1,19 @@
+#####################################################
+##                    Set up code                  ##
+## This is source on launch of the Rstudio project ##
+#####################################################
+
+#===================================
+# File paths
+
+# Check for OS
+sys_path <- startR::get_filestream_path()
+
+# Path to emLab data folder
+data_path <- file.path(sys_path,"Shared drives/emlab/data")
+
+# Path to this project folder
+project_path <- file.path(sys_path,"Shared drives/emlab/projects/current-projects/china_hs_closure")
+
+#===================================
+# Source functions
