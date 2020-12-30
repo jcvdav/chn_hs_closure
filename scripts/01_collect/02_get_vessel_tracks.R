ae06a8d181f33e0f939795bc94400c9382fcfce5
Juan Carlos Villasenor Derbez <juancarlos@ucsb.edu>
2020-12-30 15:20:37 -0800

    Updated script that collects vessel tracks from big query

--- scripts/01_collect/02_get_vessel_tracks.R
+++ scripts/01_collect/02_get_vessel_tracks.R
@@ -1,61 +1,92 @@
+# Get vessel tracks
+
+#### SET UP ###########################################################################
+# Load packages
 library(connections)
 library(bigrquery)
 library(tidyverse)
 
-con <- connection_open(bigquery(),
-                       project = "world-fishing-827",
-                       dataset = "gfw_research",
-                       billing = "emlab-gcp",
-                       use_legacy_sql = FALSE,
-                       allowLargeResults = TRUE)
+# Establish a connection
+con <- connection_open(
+  bigquery(),
+  project = "world-fishing-827",
+  dataset = "gfw_research",
+  billing = "emlab-gcp",
+  use_legacy_sql = FALSE,
+  allowLargeResults = TRUE)
 
-vessel_info <- tbl(con, "vi_ssvid_v20201209") %>% 
+# Temprary fix to some stupid dbplyr / google mismatch
+options(scipen = 20)                                          # Fix index error
+
+#### CREATE SUBQUERIES (appended with *_sq) #########################################
+
+# Get vessel info for all Chinese squid jiggers (There are 669 vessels)
+vessel_info_sq <- tbl(con, "vi_ssvid_v20201209") %>% 
   filter(on_fishing_list_best) %>% 
-  filter(best$best_flag == "CHN") %>%
-  select(ssvid)
+  filter(best$best_flag == "CHN",
+         best$best_vessel_class == "squid_jigger") %>%
+  select(ssvid)                                          # Figure out how to get the vessel info later
 
-good_segs <- tbl(con, "pipe_v20190502_segs") %>% 
+# Get good segments to eventually filter tracks
+good_segs_sq <- tbl(con, "pipe_v20190502_segs") %>% 
   filter(good_seg) %>% 
   select(seg_id)
 
-vessel_tracks <- tbl(con, "pipe_v20201001_fishing") %>% 
-  mutate(lat_bin_center = floor(lat/0.1)*0.1 + 0.05,
-         lon_bin_center = floor(lon/0.1)*0.1 + 0.05) %>% 
-  mutate(date = sql("EXTRACT(DATE FROM timestamp)")) %>% 
-  mutate(closure = case_when(c(between(lon, -60, -48) & between(lat, -44, -32)) ~ "Atlantic",
-                            c(between(lon, -110, -95) & between(lat, -5, 5))  ~ "Pacific",
-                            T ~ "None")) %>% 
-  filter(closure %in% c("Atlantic", "Pacific")) %>% 
-  filter(nnet_score == 1) %>% 
-  inner_join(good_segs, by = "seg_id") %>% 
+# Get ssvids for all Chinese squid jiggers who fished within the closures before (There are 513 vessels)
+affected_vessels_sq <- tbl(con, "pipe_v20201001_fishing") %>% 
   inner_join(vessel_info, by = "ssvid") %>% 
-  select(date, ssvid, lon, lat, lon_bin_center, lat_bin_center, hours, closure)
+  filter(nnet_score == 1) %>% 
+  filter(between(lon, -60, -48) & between(lat, -44, -32) |
+           between(lon, -110, -95) & between(lat, -5, 5)) %>% 
+  select(ssvid) %>% 
+  distinct() %>% 
+  mutate(treatment = "Treated")
 
-vessel_tracks_collected <- vessel_tracks %>% 
+# Get ssvids for Chinese squid jiggers that NEVER fished within the closures before (there are 153 vessels)
+non_affected_vessels_sq <- vessel_info %>% 
+  anti_join(affected_vessels_sq, by = "ssvid") %>% 
+  select(ssvid) %>% 
+  distinct() %>% 
+  mutate(treatment = "Control")
+
+#### COLLECT THE RELEVANT QUERIES ################################################
+
+# Collect affected vessels
+affected_vessels <- affected_vessels_sq %>% 
   collect()
 
-closure_ts <- vessel_tracks_collected %>% 
-  group_by(date, closure) %>% 
-  summarize(hours = sum(hours, na.rm = T)) %>% 
-  mutate(yday = lubridate::yday(date),
-         month = lubridate::month(date),
-         year = as.factor(lubridate::year(date)))
+# Collect non-affected vessels
+non_affected_vessels <- non_affected_vessels_sq %>% 
+  collect()
 
-ggplot(closure_ts, aes(x = yday, y = hours, color = year)) +
-  geom_point() +
-  facet_wrap(~closure, ncol = 1, scales = "free_y")
+# Create treatment groups
+treatment_groups <- rbind(affected_vessels,
+                          non_affected_vessels)
+  
+# Get all activity by Chinese Squid Jiggers, and tag the location as inside / outside the soon-to-be closure areas
+vessel_tracks <- tbl(con, "pipe_v20201001_fishing") %>% 
+  filter(nnet_score == 1,
+         sql("nnet_score IS NOT NULL")) %>% 
+  mutate(date = sql("EXTRACT(DATE FROM timestamp)")) %>% 
+  mutate(closure = case_when(c(between(lon, -60, -48) & between(lat, -44, -32)) ~ "Atlantic",
+                             c(between(lon, -110, -95) & between(lat, -5, 5))  ~ "Pacific",
+                             T ~ "None")) %>% 
+  inner_join(vessel_info_sq, by = "ssvid") %>%                                                      # Keep only Chinese Squid jiggers
+  inner_join(good_segs_sq, by = "seg_id") %>%                                                       # Keep only good segments
+  select(date, ssvid, lon, lat, hours, closure) %>% 
+  collect()
 
-grid <- vessel_tracks_collected %>% 
-  group_by(lon_bin_center, lat_bin_center) %>% 
-  summarize(hours = sum(hours, na.rm = T))
 
-ggplot(grid, aes(x = lon_bin_center, y = lat_bin_center, fill = hours)) +
-  geom_raster() +
-  scale_fill_continuous(trans = "log10")
+#### EXPORT DATA #################################################################
 
-ggplot(closure_ts, aes(x = month, y = year, fill = hours)) +
-  geom_raster() +
-  facet_wrap(~closure)
+
+saveRDS(treatment_groups,
+        file = file.path(project_data_path, "processed_data", "treatment_groups.rds"))
+
+saveRDS(vessel_tracks,
+        file = file.path(project_data_path, "processed_data", "vessel_tracks.rds"))
+
+
 
 
 
